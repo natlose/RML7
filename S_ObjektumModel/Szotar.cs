154e67655d6c13aad67d7010afe2be7a58c70aa0
natlose <natlose@gmail.hu>
2019-06-29 09:22:21 +0200

    LenyiloSzoveg, Szotar

---
+++ S_ObjektumModel/Szotar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sajat.ObjektumModel
+{
+    public class Szotar : Dictionary<string, string>
+    {
+        public List<KeyValuePair<string, string>> Bejegyzesek
+        {
+            get => this.ToList();
+        }
+    }
+}
