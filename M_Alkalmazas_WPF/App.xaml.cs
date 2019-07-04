ad308949258c74bbec46c572ec2bb27bd4a9477f
natlose <natlose@gmail.hu>
2019-07-04 21:10:28 +0200

    Stilus

--- M_Alkalmazas_WPF/App.xaml.cs
+++ M_Alkalmazas_WPF/App.xaml.cs
@@ -5,13 +5,28 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
+using System.Xml;
 
 namespace Sajat.Alkalmazas.WPF
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            StilusBetoltese(@"M_Alkalmazas_WPF_EgyeniStilus.xaml");
+        }
+
+        private void StilusBetoltese(string utvonal)
+        {
+            XmlReader XmlRead;
+            try
+            {
+                XmlRead = XmlReader.Create(utvonal);
+                Application.Current.Resources.MergedDictionaries.Add((ResourceDictionary)XamlReader.Load(XmlRead));
+                XmlRead.Close();
+            }
+            catch (Exception) { }
+        }
     }
 }
