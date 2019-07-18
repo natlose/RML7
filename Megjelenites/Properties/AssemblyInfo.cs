da0e9b774fe15a9bdf08805766175d0355d100dd
natlose <natlose@gmail.hu>
2019-07-18 10:30:35 +0200

    Összevonás Megjelenites szerelvénybe

--- Megjelenites/Properties/AssemblyInfo.cs
+++ Megjelenites/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("M_Cikk_WPF")]
+[assembly: AssemblyTitle("Megjelenites")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("M_Cikk_WPF")]
+[assembly: AssemblyProduct("Megjelenites")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
